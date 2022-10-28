bfa0f17861f7723165ff94e7e421bf848789b67a
Brandon Gustrowsky <bgustrowsky@southern.edu>
2022-10-28 15:06:21 -0400

    Swapped the order of elements in theme object

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,24 @@ function App() {
 
   // Color pallete. Please access colors from the theme object.
   const theme = createTheme({
+    palette: {
+      mode: "light",
+      primary: {
+        main: "#525E75", //gray
+      },
+      secondary: {
+        main: "#F1DDBF", //cream
+      },
+      purple : {
+        main: "#C400E4" //purple
+      },
+      green: {
+        main: "#92BA92", //green
+      },
+      gold: {
+        main: "#DAA520", //gold
+      }
+    },
     components: {
       MuiCard: {
         styleOverrides: {
@@ -19,29 +37,6 @@ function App() {
         }
       }
     },
-    palette: {
-      mode: "light",
-      primary: {
-        main: "#525E75",
-      },
-      secondary: {
-        main: "#F1DDBF",
-      },
-      purple : {
-        main: "#C400E4"
-      },
-      green: {
-        main: "#92BA92",
-      },
-      gold: {
-        main: "#DAA520",
-      }
-    }
-    // cream  : "#F1DDBF",
-    // green  : "#92BA92",
-    // gray   : "#525E75",
-    // purple : "#C400E4",
-    // gold   : "#DAA520",
   })
 
   return (
