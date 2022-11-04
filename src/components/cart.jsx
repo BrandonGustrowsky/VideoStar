c5b196451a589f697717a832b0ddb659028e58cb
ahumanchild <essyfirst@gmail.com>
2022-11-03 22:26:16 -0400

    Shows cart title

--- src/components/cart.jsx
+++ src/components/cart.jsx
@@ -1,8 +1,18 @@
 import VideoCard from './components/VideoCard'
 import NavBar from './components/NavBar'
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
+const cart = (props) => {
+    const [] = useState(null)
+}
 
+const cartM = () => {
+
+}
+
+<div id="Cart">
+    <h1 id="pageTitle">Your Cart</h1>
+</div>
 
 
 export default cart
