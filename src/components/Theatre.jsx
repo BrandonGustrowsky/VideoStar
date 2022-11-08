162b650864f2308890af1e48d6eb861a87159fa3
Brandon Gustrowsky <brandon@may21.us>
2022-11-08 17:00:57 -0500

    Added an on hover effect for Video cards

--- src/components/Theatre.jsx
+++ src/components/Theatre.jsx
@@ -51,9 +51,11 @@ const Theatre = (props) => {
             <video width="100%" height="450px" crossOrigin="true" src={ url } controls>
                 <source src={ url } type="video/mp4" alt="Video" />
             </video>
-            <section style={{display: "flex", justifyContent: "space-between", margin: "0 55px", flexWrap: "wrap"}}>
-                <Typography variant="h1" color="secondary" style={{fontWeight: 350, fontSize: "70px", fontStyle: "italic"}}> { name } </Typography>
-                <Typography variant="h2" color="secondary" style={{fontSize: "40px", lineHeight: "85px"}}> { duration } </Typography>
+            <section style={{display: "flex", flexDirection: "column", justifyContent: "space-between", margin: "0 55px", flexWrap: "wrap", gap:"15px"}}>
+                <div style={{display: "flex", justifyContent: "space-between", margin: "0 55px", gap: "45px", flexWrap: "nowrap"}}>
+                    <Typography variant="h1" color="secondary" style={{fontWeight: 350, fontSize: "70px", fontStyle: "italic"}}> { name } </Typography>
+                    <Typography variant="h2" color="secondary" style={{fontSize: "40px", lineHeight: "85px"}}> { duration } </Typography>
+                </div>
                 <Typography variant="h3" color="secondary" style={{fontSize: "35px", fontWeight: "300"}}> { size + " MB"} </Typography>
             </section>
             <section id="recommended">
--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -21,8 +21,8 @@ const VideoCard = (props) => {
     }
 
     return (
-        <Card sx={{ width: "345px", height: "300px", boxShadow: "rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px"}}>
-            <video width="auto" height="195px" src={ url } crossOrigin="true" onClick={clickedVideo} >
+        <Card className="videoCard" sx={{ width: "345px", height: "300px", boxShadow: "rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px"}}>
+            <video className="videoCardVideo" width="auto" height="195px" src={ url } crossOrigin="true" onClick={clickedVideo} >
                 <source src={ url } type="video/mp4" alt="Here!" />
             </video>
             <CardContent style={{ maxHeight: "80px"}}>
