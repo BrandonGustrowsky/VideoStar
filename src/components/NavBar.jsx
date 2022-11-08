86b1bee887fc489e75989264af05804370d872a6
Brandon Gustrowsky <brandon@may21.us>
2022-11-08 13:15:29 -0500

    Slight updates to NavBar styling

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,9 +6,9 @@ import Button from '@mui/material/Button';
 
 const Navbar = () => {
     return ( //Figure out why boxShadow isnt working
-        <header style={{"background": "#525E75", width: "100%", height: "150px", flexGrow: 1, marginBottom: "10px"}}> 
-            <Box style={{boxShadow: "0px 5px 20px 3px rgba(40,40,40,0.6)",}}>
-                <Toolbar>
+        <header style={{"background": "#525E75", width: "100%", flexGrow: 1, marginBottom: "10px"}}> 
+            <Box style={{boxShadow: "0px 5px 20px 3px rgba(20,20,20,0.6)",}}>
+                <Toolbar style={{display: "flex", justifyContent: "space-between", flexWrap: "wrap"}}>
                     <Typography variant="h1" component="div" sx={{ color: "#92BA92", flexGrow: 1, lineHeight: "150px", fontSize: "100px", fontWeight: "400", textShadow: "0px 0px 15px #000000" }}> 
                         VideoStar
                     </Typography>
