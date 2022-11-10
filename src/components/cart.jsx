8250cd92d086c9d22ac82e31157e4711bc100f3b
ahumanchild <essyfirst@gmail.com>
2022-11-09 21:22:19 -0500

    cart  build start

--- src/components/cart.jsx
+++ src/components/cart.jsx
@@ -2,6 +2,7 @@ import VideoCard from './VideoCard'
 import NavBar from './NavBar'
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import { useState, useEffect } from "react";
+import { SettingsInputAntennaTwoTone } from '@mui/icons-material';
 
 const Cart = (props) => {
     const { showingCart } = props
@@ -14,16 +15,22 @@ const Cart = (props) => {
     // const handleClick = () => {
     //     setShowCart(prevshowCart => {return !prevShowCart})
     // }
+    const [isLiked, setisLinked] = useState(null)
 
     return (
-        <div id="Cart" style={{}}>
-            <h1 id="pageTitle">Your Cart</h1>
-        </div>
+        <main style={{background: "#525E75"}}>
+            <div id="Cart" style={{background: "#525E75"}}>
+                <h1 id="pageTitle" style={{color: "#F1DDBF"}} >Your Cart <ShoppingCartIcon></ShoppingCartIcon></h1>
+            </div>
+            <div id="videoss">
+
+            </div>
+            <div id='otherinfo'>
+
+            </div>
+        </main>
     )
 }
 
-// const cartM = () => {
-
-// }
 
 export default Cart
