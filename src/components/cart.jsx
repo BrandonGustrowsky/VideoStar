900beb02e0cadff12d08ff7a0cb00269c60974eb
ahumanchild <essyfirst@gmail.com>
2022-11-09 21:52:38 -0500

    added buttons

--- src/components/cart.jsx
+++ src/components/cart.jsx
@@ -6,27 +6,23 @@ import { SettingsInputAntennaTwoTone } from '@mui/icons-material';
 
 const Cart = (props) => {
     const { showingCart } = props
-    // const [showCart, setShowCart] = useState(showingCart)
 
-    // useEffect(() => {
-    //     setShowCart(showingCart)
-    // }, [showCart])
-
-    // const handleClick = () => {
-    //     setShowCart(prevshowCart => {return !prevShowCart})
-    // }
     const [isLiked, setisLinked] = useState(null)
 
     return (
-        <main style={{background: "#525E75"}}>
+        <main id='cart' style={{background: "#525E75", height: "100%"}}>
             <div id="Cart" style={{background: "#525E75"}}>
-                <h1 id="pageTitle" style={{color: "#F1DDBF"}} >Your Cart <ShoppingCartIcon></ShoppingCartIcon></h1>
+                <h1 id="pageTitle" style={{color: "#F1DDBF", textAlign: "center", fontSize: "50px", paddingBottom: "30px"}} >Your Cart <ShoppingCartIcon style={{fontSize: "50px"}}></ShoppingCartIcon></h1>
             </div>
+            <div style={{display: "flex", flexDirection: "column"}}>
             <div id="videoss">
-
+                
             </div>
-            <div id='otherinfo'>
-
+            <div id='cartinfo'>
+                <button style={{background: "#525E75", color: "#F1DDBF", fontSize: "15px", height: "45px", width: "100px"}}>Delete All</button>
+                <h2>Total: $ </h2>
+                <button style={{background: "#525E75", color: "#F1DDBF", fontSize: "15px", height: "45px", width: "100px"}}>Purchase</button>
+            </div>
             </div>
         </main>
     )
