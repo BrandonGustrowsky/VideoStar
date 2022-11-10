f6ef36bf7bcc42c1db4807feb08c94fa76b08fde
MaxifiedFRFX <srnitro>
2022-11-09 22:36:18 -0500

    adding padding

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -143,7 +143,7 @@ const Gallery = (props) => {
     return (
 
         <div>
-            <div style={{ display: "flex", gap: "15px", padding: "5px" }}>
+            <div style={{ display: "flex", gap: "15px", padding: "10px" }}>
                 <Box sx={{ width: "100%" }}>
                     <FormControl fullWidth>
                         <InputLabel id="demo-simple-select-label">Filter</InputLabel>
