b56861e1764c5e4f6f462bf80bb5ac4e7b1d7e39
ahumanchild <essyfirst@gmail.com>
2022-11-10 22:58:05 -0500

    cart functionality and stylling

--- src/components/cart.jsx
+++ src/components/cart.jsx
@@ -15,15 +15,6 @@ const Cart = (props) => {
             return video
         })
     }
-
-    //const handleTotalPurchase = () => {
-      //  return => {
-        //    if (video.isInCart){
-          //       const eachin = data.map(video.isInCart.price)
-            //}
-        //}
-    //}
-
     
 
     const handlePurchaseVideos = () => {
@@ -36,18 +27,30 @@ const Cart = (props) => {
         })
     }
 
+        console.log(data.reduce((accumulator, video) => {
+            if (video.isInCart){
+                accumulator += video.price
+            } 
+            return accumulator
+        }, 0).toFixed(2))
+
     return (
-        <main id='root' style={{background: "#525E75", height: "100%"}}>
-            <div id="Cart" style={{background: "#525E75"}}>
+        <main id='root'  style={{background: "#525E75", height: "100%"}}>
+            <div style={{background: "#525E75"}}>
                 <h1 id="pageTitle" style={{color: "#F1DDBF", textAlign: "center", fontSize: "50px", paddingBottom: "30px", textShadow: "4px 4px 2px rgba(0,0,0,0.6)"}} >Your Cart <ShoppingCartIcon style={{fontSize: "50px", textShadow: "4px 4px 2px rgba(0,0,0,0.6)"}}></ShoppingCartIcon></h1>
             </div>
-            <div style={{display: "flex", justifyContent: "space-between", flexWrap: "wrap"}}>
-                <div style={{display: 'flex', gap:"150px", marginLeft: "250px", marginRight: "50px", flexWrap: "wrap", width: "50%"}} >
+            <div className='cart'>
+                <div style={{display: 'flex', gap:"75px", flexWrap: "wrap", width: "50%"}} >
                     {videos}
                 </div>
-                <div id='cartinfo' style={{marginRight: "200px"}}>
+                <div id='cartinfo'>
                     <button className="cartButton" style={{background: "#525E75", color: "#F1DDBF", fontSize: "15px", height: "45px", width: "100px", marginTop: "50px"}} onClick={() => {setData(handleDeleteAll)}}>Remove All</button>
-                    <h2 style={{color: "#F1DDBF", marginTop: "300px", textShadow: "4px 4px 2px rgba(0,0,0,0.6)"}}>Total: $ </h2>
+                    <h2 style={{color: "#F1DDBF", marginTop: "300px", textShadow: "4px 4px 2px rgba(0,0,0,0.6)"}}>Total: ${data.reduce((accumulator, video) => {
+                                                                                                                                                    if (video.isInCart){
+                                                                                                                                                        accumulator += video.price
+                                                                                                                                                    }
+                                                                                                                                                    return accumulator
+                                                                                                                                                }, 0).toFixed(2)} </h2>
                     <button className="cartButton" style={{background: "#525E75", color: "#F1DDBF", fontSize: "15px", height: "45px", width: "100px", marginBottom: "150px"}} onClick={() => {setData(handlePurchaseVideos)}}>Purchase</button>
                 </div>
             </div>
