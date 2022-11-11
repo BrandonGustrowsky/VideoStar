e38cf9df545fc4efd1a58cb0a3eea52e5052dd24
Brandon Gustrowsky <brandon@may21.us>
2022-11-11 09:00:10 -0500

    Theatre mode styled for mobile. Text resizes and wraps appropriately.

--- src/components/Theatre.jsx
+++ src/components/Theatre.jsx
@@ -17,10 +17,10 @@ const Theatre = (props) => {
             </video>
             <section style={{display: "flex", flexDirection: "column", justifyContent: "space-between", margin: "0 55px", flexWrap: "wrap", gap:"15px"}}>
                 <div style={{display: "flex", justifyContent: "space-between", margin: "0 55px", gap: "45px", flexWrap: "nowrap"}}>
-                    <Typography variant="h1" color="secondary" style={{fontWeight: 350, fontSize: "70px", fontStyle: "italic"}}> { video.name } </Typography>
-                    <Typography variant="h2" color="secondary" style={{fontSize: "40px", lineHeight: "85px"}}> { video.duration } </Typography>
+                    <Typography variant="h1" color="secondary" id="videoName" className="theatreModeFont"> { video.name } </Typography>
+                    <Typography variant="h2" color="secondary" id="durationFont" style={{ lineHeight: "85px"}}> { video.duration } </Typography>
                 </div>
-                <Typography variant="h3" color="secondary" style={{fontSize: "35px", fontWeight: "300"}}> { video.size + " MB"} </Typography>
+                <Typography variant="h3" color="secondary" id="sizeFont" style={{ fontWeight: "300"}}> { video.size + " MB"} </Typography>
             </section>
             <section id="recommended">
                 { (recommendedVideos.length > 0) ? recommendedVideos : <Typography variant="p" color="green" style={{fontSize: "35px", fontWeight:"bold"}}>There are no more videos to recommend. <SentimentDissatisfiedIcon /></Typography>}
